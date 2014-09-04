fefa10449c7ffd4085dd614a0100e53352331bb9
dd <xyz@abc>
2014-09-04 15:27:06 +0800

    add car, cdr, cons operator

--- cell.cpp
+++ cell.cpp
@@ -23,6 +23,8 @@ Cell *get_pair()
   Cell *c = &pair_pool[free_pair_index++];
   c->type_ = PAIR;
   c->pair_attr_ = HEAD;
+  c->first_ = 0;
+  c->second_ = 0;
   return c;
 }
 
@@ -40,9 +42,14 @@ Cell *get_cell(const char *val, CellType type)
 
 void print_cell(const Cell *cell)
 {
+  if (cell == 0)
+  {
+    cout << "null ptr" << endl;
+    return;
+  }
   if (cell->type() == PAIR)
   {
-    if (cell->pair_attr_ == HEAD)
+    if (cell->pair_attr_ == FIRST || cell->pair_attr_ == HEAD)
       cout << "( "; 
     print_cell(cell->first_);
     print_cell(cell->second_);
@@ -52,15 +59,201 @@ void print_cell(const Cell *cell)
     if (cell->type() != NULL_CELL)
       cout << cell->val_ << " , ";
     else
-      cout << " () )" << endl;
+      cout << " () ) ";
   }
+}
+
+const int MAX_LINE_LEN = 1000;
+
+#define LEN_TOO_SHORT -1
+#define OK 0
+
+int add_space_to_parenthesis(const char *org_str, char *new_str, int new_str_size)
+{
+  const char *c = org_str;
+  int i=0;
+
+  while (*c)
+  {
+    if (*c == '(' || *c == ')')
+    {
+      if (i+3 > new_str_size)
+        return LEN_TOO_SHORT;
+      new_str[i++] = ' ';
+      new_str[i++] = *c;
+      new_str[i++] = ' ';
+    }
+    else
+    {
+      if (i+1 > new_str_size)
+        return LEN_TOO_SHORT;
+      new_str[i++] = *c;
+    }
+    ++c;
+  }
+  new_str[i] = '\0';
+  
+  return OK;
+}
+
+Cell *create_expression(const char *exp)
+{
+  char val[MAX_SIZE]; 
+  const char *c = exp;
+  int i=0;
+  Cell *head;
+  Cell *prev_pair;
+  Cell *cell;
+  Cell *pair;
+
+  while(*c)
+  {
+    while (*c == ' ') // skip blank
+      ++c;
+    if (*c == '(')
+    {
+      pair = get_pair();
+      pair->pair_attr_ = HEAD;
+      head = prev_pair = pair;
+
+      ++c;
+      while (*c == ' ') // skip blank
+        ++c;
+    }
+
+
+    while (*c != ' ' && *c != '(' && *c != ')')
+      val[i++] = *c++;
+    val[i] = '\0';
+    cout << val << endl;
+
+    cell = get_cell(val, SYMBOL);
+
+    pair->first_ = cell;
+    pair->second_ = &null_cell;
+
+
+    if (prev_pair != pair)
+    {
+      cout << "add to prev_pair" << endl;
+
+      pair->pair_attr_ = SECOND;
+      prev_pair->second_ = pair;
+      prev_pair = pair;
+    }
+    pair = get_pair();
+
+    i=0;
+    if (*c == ')')
+    {
+      cout << ")))" << endl;
+      return head;
+    }
+    ++c;
+  }
+  return head;
+}
+
+Cell *car_cell(Cell *cell)
+{
+  if (cell->type_ != PAIR)
+  {
+    strcpy(invalid_cell.val_, "not pair");
+    return &invalid_cell;
+  }
+  else
+    return cell->first_; // first_ should not be 0.
+}
+
+Cell *cdr_cell(Cell *cell)
+{
+  if (cell->type_ != PAIR)
+  {
+    strcpy(invalid_cell.val_, "not pair");
+    return &invalid_cell;
+  }
+  else
+    return cell->second_; // second_ should not be 0.
+}
+
+Cell *cons_cell(Cell *first, Cell *second)
+{
+  Cell *pair = get_pair();
+  pair->first_ = first; 
+  pair->second_ = second;
+  if (first->type_ == PAIR)
+    first->pair_attr_ = FIRST;
+  if (second->type_ == PAIR)
+    second->pair_attr_ = SECOND;
+
+  return pair; // PairAttr not set.
+}
+
+
+Cell *make_exp()
+{
+  Cell *cell = get_cell("5", SYMBOL);
+  Cell *new_cell = cons_cell(cell, &null_cell);
+  new_cell->pair_attr_ = HEAD;
+
+  cell = get_cell("3", SYMBOL);
+  Cell *c1 = cons_cell(cell, new_cell);
+
+  cell = get_cell("*", SYMBOL);
+  Cell *c2 = cons_cell(cell, c1);
+
+  return c2;
 }
 
 #ifdef TEST_CELL
 int main(int argc, char *argv[])
 {
+  char input_str[MAX_LINE_LEN];
+  char for_split[MAX_LINE_LEN]; // appned ' ' for ( or ): ex "(" => " ( ", ")" => " ) "
   invalid_cell.type_ = INVALID;
   null_cell.type_ = NULL_CELL;
+
+  Cell *exp = make_exp();
+  print_cell(exp);
+
+  Cell *cell = get_cell("2", SYMBOL);
+  Cell *new_cell = cons_cell(cell, &null_cell);
+  new_cell->pair_attr_ = HEAD;
+
+  print_cell(new_cell);
+
+  cell = get_cell("1", SYMBOL);
+  Cell *c1 = cons_cell(exp, new_cell);
+  cout << "\n-----" << endl;
+  print_cell(c1);
+  cout << "\n-----" << endl;
+
+  cell = get_cell("+", SYMBOL);
+  Cell *c2 = cons_cell(cell, c1);
+  cout << "\n-----" << endl;
+  print_cell(c2);
+  cout << "\n-----" << endl;
+
+
+#if 0
+  strcpy(input_str, "(+ 123 789)");
+
+  Cell *head = create_expression(input_str);
+  cout << "free_pair_index: " << free_pair_index << endl;
+  cout << "free_cell_index: " << free_cell_index << endl;
+  print_cell(head);
+  cout << endl;
+
+  //strcpy(input_str, "(+ (* 6 7) 2)");
+  int r = add_space_to_parenthesis(input_str, for_split, MAX_LINE_LEN);
+  cout << input_str << endl;
+  if (r==OK)
+    cout << for_split << endl;
+  else
+    cout << r << endl;
+
+#endif
+#if 0
   // (+ 1 2), append null_cell
   Cell *c = get_cell("+", SYMBOL);
   Cell *pair = get_pair();
@@ -93,7 +286,7 @@ int main(int argc, char *argv[])
   iter_cell = pair;
 
   print_cell(head);
-
+#endif
   return 0;
 }
 #endif
