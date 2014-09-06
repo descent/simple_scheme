26f3751a7192d40da1a8ac5b4227da400e7938ba
dd <xyz@abc>
2014-09-06 22:16:55 +0800

    add lambda_cell

--- s_eval.cpp
+++ s_eval.cpp
@@ -22,6 +22,7 @@ bool isdig(char c) { return isdigit(static_cast<unsigned char>(c)) != 0; }
 
 Cell invalid_cell;
 Cell null_cell;
+Cell lambda_cell;
 
 struct Environment;
 
@@ -859,6 +860,8 @@ int main ()
 {
   invalid_cell.type_ = INVALID;
   null_cell.type_ = NULL_CELL;
+  lambda_cell.type_ = SYMBOL;
+  strcpy(lambda_cell.val_, "lambda");
 
   Environment global_env; //add_globals(global_env);
   create_primitive_procedure(global_env.frame_);
