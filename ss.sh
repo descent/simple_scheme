c580fdb26987266f62b5ef6e8530c18730c2ede7
descent <descent0@gmail.com>
2014-11-17 11:33:55 +0800

    new file:   ss.sh

---
+++ ss.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s /home/descent/git/progs/queue/gdeque.h .
+ln -s /home/descent/git/progs/queue/gdeque.cpp .
+
+ln -s /home/descent/git/progs/queue/cstring.h .
+ln -s /home/descent/git/progs/queue/cstring.cpp .
+
