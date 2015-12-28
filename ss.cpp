0ae6d55e9ff23e0eaed65d374b727c1cf35117a4
descent <descent0@gmail.com>
2015-12-28 10:01:04 +0800

    add P103 macro for p103 version

--- ss.cpp
+++ ss.cpp
@@ -1,10 +1,19 @@
 #include "s_eval.h"
 
+#ifdef P103
+#define main(...) mymain()
+#endif
+
 int main(int argc, char *argv[])
 {
   init_eval();
   Environment *global_env = get_env(0, "global");
   create_primitive_procedure(global_env);
+
+#ifdef P103
+  non_os_repl("simple scheme> ", global_env);
+#else
   repl("simple scheme> ", global_env);
+#endif
   return 0;
 }
