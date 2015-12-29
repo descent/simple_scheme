1cdc5e2403a2659882a0828dd3c9bffccae757a0
descent <descent0@gmail.com>
2015-12-29 11:49:37 +0800

    new file:   rpi2/ln.sh 	new file:   rpi2/makefile 	new file:   rpi2/readme 	new file:   rpi2/rpi2.ld 	new file:   rpi2/rule.mk

---
+++ rpi2/rule.mk
@@ -0,0 +1,8 @@
+CFLAGS=-g
+MYCFLAGS=-fno-common -O0 -g -mcpu=cortex-a7 -I./inc -DRPI2
+MYCXXFLAGS = -fno-exceptions -fno-rtti -ffreestanding -nostdlib -nodefaultlibs -std=c++11
+LD_FLAGS=-Wl,-T./rpi2.ld -Wl,--build-id=none -nostartfiles
+CXX=arm-none-eabi-g++
+%.dpp: %.cpp
+	set -e; rm -f $@ ; $(CXX) -std=c++11 -MM $(MYCFLAGS) $< > $@.$$$$ ; sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@ ; rm -f $@.$$$$
+
