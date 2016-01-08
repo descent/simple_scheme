4147eb4bf4dde35ebd36a12cef3ec0184248f002
descent <descent0@gmail.com>
2016-01-08 22:46:17 +0800

    new file:   stm32f407/ln.sh 	new file:   stm32f407/makefile 	new file:   stm32f407/rule.mk 	new file:   stm32f407/stm32.ld

---
+++ stm32f407/ln.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -sf ../cell.cpp .
+ln -sf ../cell.h .
+ln -sf ../ss.cpp mymain.cpp
+ln -sf ../s_eval.cpp .
+ln -sf ../s_eval.h .
+ln -sf ../token_container.cpp .
+ln -sf ../token_container.h .
