b5e72a08894e625f71d7b963c9b230a52226c90b
descent <descent0@gmail.com>
2016-04-08 13:43:32 +0800

    fixed typo define

--- ss.cpp
+++ ss.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
   Environment *global_env = get_env(0, "global");
   create_primitive_procedure(global_env);
 
-#if defined(P103) || defined(RPI2) || defined(STM32F407) || defined(X86_16) || define(UEFI)
+#if defined(P103) || defined(RPI2) || defined(STM32F407) || defined(X86_16) || defined(UEFI)
   non_os_repl("simple scheme> ", global_env);
 #else
   REPL("simple scheme> ", global_env);
