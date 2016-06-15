f64e67610b68ab503b6796f61b9b054086222b24
descent <descent0@gmail.com>
2016-06-15 13:20:25 +0800

    checkout simple_stdcpplib.git

--- rpi2/ln.sh
+++ rpi2/ln.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+CPPLIB=simple_stdcpplib
+
+if [ ! -d "$CPPLIB" ]; then
+  git clone https://github.com/descent/simple_stdcpplib.git
+  cd $CPPLIB
+  make RPI2=1
+  cd -
+fi
 ln -sf ../cell.cpp .
 ln -sf ../cell.h .
 ln -sf ../ss.cpp mymain.cpp
@@ -6,3 +15,4 @@ ln -sf ../s_eval.cpp .
 ln -sf ../s_eval.h .
 ln -sf ../token_container.cpp .
 ln -sf ../token_container.h .
+ln -sf ../x86_16.h .
