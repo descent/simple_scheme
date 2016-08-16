64da4e1c30cb659f3c34cbb6e4e57786821ae32d
descent <descent0@gmail.com>
2016-08-16 10:45:26 +0800

    modify symbolic link files

--- rpi2/ln.sh
+++ rpi2/ln.sh
@@ -9,10 +9,7 @@ if [ ! -d "$CPPLIB" ]; then
   cd -
 fi
 ln -sf ../cell.cpp .
-ln -sf ../cell.h .
 ln -sf ../ss.cpp mymain.cpp
 ln -sf ../s_eval.cpp .
-ln -sf ../s_eval.h .
 ln -sf ../token_container.cpp .
-ln -sf ../token_container.h .
-ln -sf ../x86_16.h .
+ln -sf ../*.h .
