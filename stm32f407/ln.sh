b0488e0fba5f46e64fa21252d703f77a85061642
descent <descent0@gmail.com>
2016-09-13 11:57:39 +0800

    checkout simple_stdcpplib.git, ln ../x86_16.h

--- stm32f407/ln.sh
+++ stm32f407/ln.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+CPPLIB=simple_stdcpplib
+
+if [ ! -d "$CPPLIB" ]; then
+  git clone https://github.com/descent/simple_stdcpplib.git
+  cd $CPPLIB
+  make STM32F407=1
+  cd -
+fi
 ln -sf ../cell.cpp .
 ln -sf ../cell.h .
 ln -sf ../ss.cpp mymain.cpp
@@ -6,3 +14,4 @@ ln -sf ../s_eval.cpp .
 ln -sf ../s_eval.h .
 ln -sf ../token_container.cpp .
 ln -sf ../token_container.h .
+ln -sf ../x86_16.h .
